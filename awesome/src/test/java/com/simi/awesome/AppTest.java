1ca7fa009a2968a15a95c731517a88ecffd51a6b
simipro <sihuber@gmx.net>
2013-07-17 23:07:17 +0200

    initial commit

---
+++ awesome/src/test/java/com/simi/awesome/AppTest.java
@@ -0,0 +1,29 @@
+package com.simi.awesome;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(JUnit4.class)
+public class AppTest {
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest() {
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		Assert.assertTrue(true);
+	}
+}
